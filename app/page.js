b7c59bb9cce922c91b1392d32d3a7c09fc575703
Tarun Sai Srinivas <tarunsaisrinivas4@gmail.com>
2025-03-16 12:15:50 +0530

    updated

--- app/page.js
+++ app/page.js
@@ -12,7 +12,7 @@ const page = () => {
   return (
     <>
     <div>
-    <SplashCursor />
+    {/* <SplashCursor /> */}
       <Navbar />
       <HeroSection />
       <SkillsMarquee />
--- app/pages/HeroSection.jsx
+++ app/pages/HeroSection.jsx
@@ -18,43 +18,48 @@ const HeroSection = () => {
         <SequentialHyperText />
 
         <p className="mt-2 text-md text-gray-600 dark:text-gray-400 max-w-md">
-          I am passionate about building impactful applications that enhance user experience and solve real-world challenges. My expertise spans various technologies, allowing me to contribute effectively to any project.
+          I am passionate about building impactful applications that enhance
+          user experience and solve real-world challenges. My expertise spans
+          various technologies, allowing me to contribute effectively to any
+          project.
         </p>
 
         <div className="mt-6 flex flex-col md:flex-row md:space-x-4">
-         
-
           <div className="flex flex-row md:flex-row md:space-x-4 mt-4">
             <a href="https://github.com/Tarunsaisrinivas">
-            <ShimmerButton className="shadow-2xl flex items-center mb-2 md:mb-0">
-              <FaGithub className="dark:text-white mr-2" /> {/* GitHub Icon */}
-              <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white">
-                GitHub
-              </span>
-            </ShimmerButton>
+              <ShimmerButton className="shadow-2xl flex items-center mb-2 md:mb-0">
+                <FaGithub className="dark:text-white mr-2" />{" "}
+                {/* GitHub Icon */}
+                <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white">
+                  GitHub
+                </span>
+              </ShimmerButton>
             </a>
             <a href="https://www.linkedin.com/in/tarun-sai-srinivas">
-            <ShimmerButton className="shadow-2xl flex items-center mb-2 md:mb-0">
-              <FaLinkedin className="dark:text-white mr-2" /> {/* LinkedIn Icon */}
-              <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white">
-                LinkedIn
-              </span>
-            </ShimmerButton>
+              <ShimmerButton className="shadow-2xl flex items-center mb-2 md:mb-0">
+                <FaLinkedin className="dark:text-white mr-2" />{" "}
+                {/* LinkedIn Icon */}
+                <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white">
+                  LinkedIn
+                </span>
+              </ShimmerButton>
             </a>
-            <a href='/public/Resume.pdf' download="A Tarun Sai Srinivas">        
-            <ShimmerButton className="shadow-2xl flex items-center">
-            <FaFileAlt className="dark:text-white mr-2" />
-              <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white">
-                Resume
-              </span>
-            </ShimmerButton>
+            <a
+              href="https://drive.google.com/file/d/1VpmExF5jBbhZ1Gz1tCb4NPEH2g8r6EZD/view?usp=sharing"
+              target="_blank"
+            >
+              <ShimmerButton className="shadow-2xl flex items-center">
+                <FaFileAlt className="dark:text-white mr-2" />
+                <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white">
+                  Resume
+                </span>
+              </ShimmerButton>
             </a>
           </div>
         </div>
       </div>
       <div className="md:w-1/2 w-full  flex md:h-auto  justify-center md:justify-end">
-        <Globe  />
-     
+        <Globe />
       </div>
     </div>
   );
